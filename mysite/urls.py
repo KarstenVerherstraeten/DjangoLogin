485f2f809947db3190a23b87dfa152a8d79fd7dd
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-12 11:29:23 +0100

    Update URL routing to use Django's built-in authentication views

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('login/', include('login.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
