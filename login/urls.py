b9470feaa9c88664f7be4956de8f5c6654a7e02c
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-12 11:15:09 +0100

    Add registration functionality with template and URL routing

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "login"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
 ]
