e506549995759cf63f2f88ccb0625ef850da34a4
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-13 09:49:04 +0100

    Refactor registration form to use custom fields and updated the register view

--- login/views.py
+++ login/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib import messages
+from .forms import CustomRegistrationForm
 
 def index(request):
     return render(request, "login/index.html")
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log in the user after registration
             messages.success(request, "Account created successfully!")
             return redirect('home')  # Redirect after registration
     else:
-        form = UserCreationForm()
+        form = CustomRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
