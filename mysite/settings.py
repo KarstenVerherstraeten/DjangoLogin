b2faa0e8f02848ab03c05094f9f080577551fbdf
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-13 11:30:58 +0100

    Add LOGIN_REDIRECT_URL setting to settings.py for post-login redirection

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'login:dashboard'  # Redirects to home after login
 LOGOUT_REDIRECT_URL = '/'  # Redirects to home after logout
 
 # Default primary key field type
