e508903425c01798ba6b5bf153e83bd561473512
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-11-13 11:19:51 +0100

    Add dashboard view and update registration redirect

--- login/views.py
+++ login/views.py
@@ -13,7 +13,11 @@ def register(request):
             user = form.save()
             login(request, user)  # Log in the user after registration
             messages.success(request, "Account created successfully!")
-            return redirect('home')  # Redirect after registration
+            return redirect('login:dashboard')  # Redirect after registration
     else:
         form = CustomRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
